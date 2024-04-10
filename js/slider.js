a2284f92419cf657b26ccd7e014fd1d0a1d8667b
tcphann <157539491+tcphann@users.noreply.github.com>
2024-04-09 22:05:35 -0500

    added

---
+++ js/slider.js
@@ -0,0 +1,24 @@
+let currentSlide = 0;
+
+        function showSlide(slideIndex) {
+            const slides = document.getElementsByClassName("slide");
+            if (slideIndex >= slides.length) { currentSlide = 0; }
+            if (slideIndex < 0) { currentSlide = slides.length - 1; }
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slides[currentSlide].style.display = "block";
+        }
+
+        function prevSlide() {
+            showSlide(currentSlide -= 1);
+        }
+
+        function nextSlide() {
+            showSlide(currentSlide += 1);
+        }
+
+        document.addEventListener("DOMContentLoaded", function () {
+            showSlide(currentSlide);
+        });
+  
