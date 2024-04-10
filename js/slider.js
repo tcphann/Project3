bb79615b59c12c750e0c8e7bfd05a9cbd50403f5
tcphann <157539491+tcphann@users.noreply.github.com>
2024-04-09 23:08:14 -0500

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,23 @@
 let currentSlide = 0;
 
-        function showSlide(slideIndex) {
-            const slides = document.getElementsByClassName("slide");
-            if (slideIndex >= slides.length) { currentSlide = 0; }
-            if (slideIndex < 0) { currentSlide = slides.length - 1; }
-            for (let i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            slides[currentSlide].style.display = "block";
-        }
+function showSlide(slideIndex) {
+    const slides = document.getElementsByClassName("slide");
+    if (slideIndex >= slides.length) { currentSlide = 0; }
+    if (slideIndex < 0) { currentSlide = slides.length - 1; }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[currentSlide].style.display = "block";
+}
 
-        function prevSlide() {
-            showSlide(currentSlide -= 1);
-        }
+function prevSlide() {
+    showSlide(currentSlide -= 1);
+}
 
-        function nextSlide() {
-            showSlide(currentSlide += 1);
-        }
+function nextSlide() {
+    showSlide(currentSlide += 1);
+}
 
-        document.addEventListener("DOMContentLoaded", function () {
-            showSlide(currentSlide);
-        });
-  
+document.addEventListener("DOMContentLoaded", function () {
+    showSlide(currentSlide);
+});
